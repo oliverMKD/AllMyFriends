3684702eb519696a672478eae263335a5e4fb9c7
oliverMKD <oliverdamjan2013@gmail.com>
2017-12-01 18:54:53 +0100

    First

--- app/src/main/java/com/oli/allmyfriends/Main2Activity.java
+++ app/src/main/java/com/oli/allmyfriends/Main2Activity.java
@@ -44,6 +44,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
         ButterKnife.bind(this);
         podatoci = new UserDetails();
 
